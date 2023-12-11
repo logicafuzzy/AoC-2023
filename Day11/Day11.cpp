cfaa342fffabcd12369bfab151bb6c01cc6aeef3
Michele Antolini <michele.antolini.it@gmail.com>
2023-12-11 07:45:11 +0100

    Day11 part2

--- Day11/Day11.cpp
+++ Day11/Day11.cpp
@@ -14,17 +14,17 @@ struct galaxy_t {
 	int col{};
 };
 
-bool all_equal(const row_t& row) {
+bool empty_space(const row_t& row) {
 	for (auto val : row)
-		if (val != row[0])
+		if (val != '.' && val != '-')
 			return false;
 
 	return true;
 }
 
-bool all_equal(const map_t& map, int col) {
+bool empty_space(const map_t& map, int col) {
 	for (const row_t& row : map)
-		if (row[col] != map[0][col])
+		if (row[col] != '.' && row[col] != '-')
 			return false;
 
 	return true;
@@ -62,7 +62,7 @@ void expand(map_t& map) {
 	//expand rows
 	vector<int> to_expand;
 	for (int i = 0; i < map.size(); ++i)
-		if (all_equal(map[i]))
+		if (empty_space(map[i]))
 			to_expand.push_back(i);
 
 	for (int i = 0; i < to_expand.size(); ++i)
@@ -71,12 +71,97 @@ void expand(map_t& map) {
 	//expand cols
 	to_expand.clear();
 	for (int i = 0; i < map[0].size(); ++i)
-		if (all_equal(map, i))
+		if (empty_space(map, i))
 			to_expand.push_back(i);
 
 	for (int i = 0; i < to_expand.size(); ++i)
 		expand_col(map, to_expand[i] + i);
 
+}
+
+void expand2(map_t& map) {
+	//expand rows
+	for (int i = 0; i < map.size(); ++i)
+		if (empty_space(map[i]))
+			for (auto& v : map[i])
+				v = '-';
+
+	//expand cols
+	for (int i = 0; i < map[0].size(); ++i)
+		if (empty_space(map, i))
+			for (row_t& row : map)
+				row[i] = '-';
+}
+
+
+int count(const row_t& row, char c, int min, int max) {
+	int res = 0;
+	for (int i = min; i < max; i++)
+		if (row[i] == c)
+			res++;
+
+	return res;
+}
+
+int count(const map_t& map, int col, char c, int min, int max) {
+	int res = 0;
+	for (int i = min; i < max; i++)
+		if (map[i][col] == c)
+			res++;
+
+	return res;
+}
+
+void part1(map_t map) {
+	expand(map);
+
+	vector<galaxy_t> galaxies = get_galaxies(map);
+
+	long sum = 0;
+
+	for (int i = 0; i < galaxies.size(); ++i) {
+
+		for (int j = i + 1; j < galaxies.size(); ++j) {
+
+			int dist = abs(galaxies[i].row - galaxies[j].row) + abs(galaxies[i].col - galaxies[j].col);
+
+			sum += dist;
+
+			//printf("distance %d %d: %d\n", i + 1, j + 1, dist);
+
+		}
+		//sum += min_dist;
+	}
+
+	printf("Sum min_dist: %d\n", sum);
+}
+
+void part2(map_t map) {
+	expand2(map);
+
+	vector<galaxy_t> galaxies = get_galaxies(map);
+
+	uint64_t sum = 0;
+	constexpr long expansion = 1000000;
+
+	for (int i = 0; i < galaxies.size(); ++i) {
+
+		for (int j = i + 1; j < galaxies.size(); ++j) {
+			long col_expansion = count(map[galaxies[i].row], '-', min(galaxies[i].col, galaxies[j].col), max(galaxies[i].col, galaxies[j].col));
+			long row_expansion = count(map, galaxies[i].col, '-', min(galaxies[i].row, galaxies[j].row), max(galaxies[i].row, galaxies[j].row));
+
+			uint64_t dist = abs(galaxies[i].row - galaxies[j].row) + abs(galaxies[i].col - galaxies[j].col)
+				+ (col_expansion + row_expansion) * expansion - col_expansion - row_expansion;
+
+			sum += dist;
+
+			//printf("distance %d %d: %d\n", i + 1, j + 1, dist);
+
+		}
+		//sum += min_dist;
+	}
+
+	printf("Sum min_dist: %lld\n", sum);
 }
 
 int main() {
@@ -100,27 +185,9 @@ int main() {
 
 	input.close();
 
-	expand(map);
+	//part1(map);
 
-	vector<galaxy_t> galaxies = get_galaxies(map);
-
-	long sum = 0;
-
-	for (int i = 0; i < galaxies.size(); ++i) {
-
-		for (int j = i + 1; j < galaxies.size(); ++j) {
-
-			int dist = abs(galaxies[i].row - galaxies[j].row) + abs(galaxies[i].col - galaxies[j].col);
-
-			sum += dist;
-
-			//printf("distance %d %d: %d\n", i + 1, j + 1, dist);
-
-		}
-		//sum += min_dist;
-	}
-
-	printf("Sum min_dist: %d\n", sum);
+	part2(map);
 
 	return 0;
 }
