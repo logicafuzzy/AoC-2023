1729ddcb03f632a59b691f088c5f7a96cddd6af5
Michele Antolini <michele.antolini.it@gmail.com>
2023-12-14 06:26:04 +0100

    Day 14 part1

--- Day14/Day14.cpp
+++ Day14/Day14.cpp
@@ -2,27 +2,53 @@
 
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	cout << " AoC 2023 Day00" << endl;
+using num_t = char;
+using row_t = vector<num_t>;
+using map_t = vector<row_t>;
 
-	ifstream input("Day00.txt");
+void move_north(map_t& map, int row) {
+	for (int r = row; r > 0; r--) {
+		for (int c = 0; c < map[0].size(); c++) {
+			if (map[r][c] == 'O' && map[r - 1][c] == '.') {
+				map[r][c] = '.';
+				map[r - 1][c] = 'O';
+			}
+		}
+	}
+}
+
+int main() {
+	cout << " AoC 2023 Day14" << endl;
+
+	ifstream input("Day14.txt");
+
+	map_t map;
 
 	while (!input.eof()) {
 		string line;
 		getline(input, line);
-		std::stringstream sline(line);
 
-		int a, b, c, d, e;
-		sline >> a >> b >> c >> d >> e;
-
-		cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+		row_t row(line.begin(), line.end());
+		map.push_back(row);
 	}
 
 	input.close();
+
+	for (int r = 1; r < map.size(); ++r)
+		move_north(map, r);
+
+	uint64_t count = 0;
+
+	for (int r = 0, w = map.size(); r < map.size(); r++, w--) {
+		count += std::count(map[r].begin(), map[r].end(), 'O') * w;
+	}
+
+	cout << "Weight: " << count << endl;
 
 	return 0;
 }
