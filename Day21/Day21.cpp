8a59bb2a3a4307bdd062e4337e852a09d52400bb
Michele Antolini <mantolini@seavision-group.com>
2023-12-21 07:45:08 +0100

    Day21 part2

--- Day21/Day21.cpp
+++ Day21/Day21.cpp
@@ -14,13 +14,6 @@ using cell_t = char;
 using row_t = vector<cell_t>;
 using map_t = vector<row_t>;
 
-//part1
-constexpr int nsteps = 64;
-constexpr bool wrap = false;
-//part2
-//constexpr int nsteps = 26501365;
-//constexpr bool wrap = true;
-
 bool is_outside(const map_t& map, const coord_t& coord) {
 	int row = coord.first, col = coord.second;
 	return row < 0 || row >= map.size() || col < 0 || col >= map[0].size();
@@ -30,8 +23,22 @@ bool is_outside(const map_t& map, int row, int col) {
 	return row < 0 || row >= map.size() || col < 0 || col >= map[0].size();
 }
 
-bool can_move(const map_t& map, int row, int col) {
-	return !is_outside(map, row, col) && map[row][col] == '.';
+int wrap_coord(int val, int wrap) {
+	if (val >= 0)
+		return val % wrap;
+	else
+		return (wrap - 1) - ((-val - 1) % wrap);
+}
+
+bool can_move(const map_t& map, int row, int col, bool wrap = false) {
+	if (wrap) {
+		row = wrap_coord(row, map.size());
+		col = wrap_coord(col, map[0].size());
+		char c = map[row][col];
+		return c == '.' || c == 'S';
+	}
+	else
+		return !is_outside(map, row, col) && (map[row][col] == '.' || map[row][col] == 'S');
 }
 
 void print_path(map_t& map, const set<coord_t>& to_check, const set<coord_t> newsteps) {
@@ -94,43 +101,54 @@ int main() {
 
 	input.close();
 
-	set<coord_t> steps{ start };
+	set<coord_t> to_check{ start };
 
-	set<coord_t> to_check = steps;
+	printf("Map size(%lu, %lu)\n", map.size(), map[0].size());
+
+	//part1
+	//constexpr int nsteps = 64;
+	//constexpr bool wrap = false;
+	//part2
+	constexpr int nsteps = 26501365;
+	constexpr bool wrap = true;
+
+	int prev_perc = 0;
 
 	for (int i = 0; i < nsteps; i++) {
+		if (10000 * i / nsteps > prev_perc) {
+			prev_perc = i / nsteps;
+			printf("step %d of %d    \r", i, nsteps);
+		}
 		set<coord_t> newsteps;
 		for (const coord_t& step : to_check) {
 			int nextrow, nextcol;
 			int row = step.first, col = step.second;
 
 			nextrow = row - 1; nextcol = col;
-			if (can_move(map, nextrow, nextcol))
+			if (can_move(map, nextrow, nextcol, wrap))
 				newsteps.insert({ nextrow, nextcol });
 
 			nextrow = row + 1; nextcol = col;
-			if (can_move(map, nextrow, nextcol))
+			if (can_move(map, nextrow, nextcol, wrap))
 				newsteps.insert({ nextrow, nextcol });
 
 			nextrow = row; nextcol = col - 1;
-			if (can_move(map, nextrow, nextcol))
+			if (can_move(map, nextrow, nextcol, wrap))
 				newsteps.insert({ nextrow, nextcol });
 
 			nextrow = row; nextcol = col + 1;
-			if (can_move(map, nextrow, nextcol))
+			if (can_move(map, nextrow, nextcol, wrap))
 				newsteps.insert({ nextrow, nextcol });
 		}
 		
 		//print_path(map, to_check, newsteps);
 
-		steps.insert(to_check.begin(), to_check.end());
-		to_check = newsteps;
+		to_check.swap(newsteps);
 	}
-	steps.insert(to_check.begin(), to_check.end());
 
-	print_path(map, to_check, {});
+	//print_path(map, to_check, {});
 
-	cout << "Result: " << to_check.size() + 1 << endl;
+	cout << endl << "Result: " << to_check.size() << endl;
 
 	return 0;
 }
