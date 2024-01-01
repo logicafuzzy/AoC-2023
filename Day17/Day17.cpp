c919a9bc0a0f40e769d3398c3719f207759aa096
Michele Antolini <mantolini@seavision-group.com>
2024-01-01 17:41:55 +0100

    Day17 WIP

--- Day17/Day17.cpp
+++ Day17/Day17.cpp
@@ -11,6 +11,7 @@
 #include <queue>
 #include <numeric>
 #include <cstdlib>
+#include <set>
 
 using namespace std;
 
@@ -29,7 +30,7 @@ enum class direction_t {
 };
 
 using link_t = pair<int, int>;
-using adj_t = vector<link_t>;
+using adj_t = set<link_t>;
 
 inline int get_id(int row, int col, int col_size) {
 	return row * col_size + col;
@@ -58,44 +59,27 @@ direction_t get_dir(int from, int to, int col_size) {
 
 }
 
-bool can_move(vector<int> prev, int from, int to, int col_size) {
-	//don't want to find max_consecutive_moves same directions 
-	bool result = true;
-	direction_t dir = get_dir(from, to, col_size);
+vector<int> get_path(int src, int tar, vector<int> const& prev) {
+	vector<int> path;
 
-	prev[to] = from;
+	for (int v = tar; v != src; v = prev[v])
+		path.push_back(v);
 
-	for (int i = 0; i <= max_consecutive_moves; ++i) {
-		if (prev[to] == -1)
-			return true;
+	path.push_back(src);
 
-		result = result && get_dir(prev[to], to, col_size) == dir;
-		to = prev[to];
-	}
-
-	return !result;
-
+	reverse(path.begin(), path.end());
+	return path;
 }
 
 void print_path(map_t map, const vector<int>& prev, int target) {
 	printf("Path: \n");
-	//printf("Can move: ");
-	int id = target;
 	
-	int path = 0;
+	vector<int> path = get_path(0, target, prev);
 
-	while (id != 0) {
-		if (id == -1)
-			continue;
-
-		//cout << can_move(prev, prev[id], id, col_size);
-
+	for (int id : path) {
 		auto coords = get_coords(id, col_size);
-		map[coords.first][coords.second] = -map[coords.first][coords.second] ;
-		id = prev[id];
+		map[coords.first][coords.second] = -map[coords.first][coords.second];
 	}
-
-	//cout << endl;
 
 	for (const auto& row : map) {
 		cout << " ";
@@ -121,58 +105,123 @@ class graph_t {
 public:
 	graph_t(int N) 
 		: _n(N) 
-		, dist(_n, numeric_limits<int>::max())
-		, prev(_n, -1)
-		, visited(_n, false)
+		, _dist(_n, numeric_limits<int>::max())
+		, _prev(_n, -1)
+		, _visited(_n, false)
+		, _col_size(0)
 	{};
 
 	void add(int from, int to, int weight) {
-		adj[from].push_back({ to, weight });
+		adj[from].insert({ to, weight });
 	}
+
+	int min_dist_index() {
+		int min_dist = numeric_limits<int>::max();
+		int min_index = 0;
+
+		for (int to = 0; to < _n; to++) {
+			if (!_visited[to]
+				&& _dist[to] <= min_dist) {
+
+				min_dist = _dist[to];
+				min_index = to;
+			}
+		}
+
+		return min_index;
+	}
+
+	int count_same_dir(int from, int to) {
+		vector<int> prev = _prev;
+
+		direction_t dir = get_dir(from, to, _col_size);
+
+		prev[to] = from;
+
+		int same_dir = 0;
+
+		for (int i = 0; i <= max_consecutive_moves; ++i) {
+			if (prev[to] == -1)
+				return same_dir;
+
+			if (get_dir(prev[to], to, _col_size) == dir)
+				same_dir++;
+			else
+				return same_dir;
+
+			to = prev[to];
+		}
+
+		return same_dir;
+	}
+
+	bool can_move(int from, int to) {
+		return count_same_dir(from, to) <= max_consecutive_moves;
+	}
+
+	//get adjacency considering max_same_direction_count
+	adj_t get_adj(int from) {
+		adj_t res;
+
+		for (auto& link : adj[from]) {
+			int to = link.first;
+			int same_dir = count_same_dir(from, to);
+			int weight = link.second;
+
+			if (can_move(from, to)) {
+				res.insert({to, weight});
+			}
+		}
+
+		return res;
+	}
+
 
 	// output shortest path from src node id
 	// adaptation of dijstra algorithm
 	vector<int> path(int src)
 	{
-		dist[src] = 0;
+		_dist[src] = 0;
 
-		int current = src;
+		for (int i = 0; i < _n; i++) {
+			int from = min_dist_index();
 
-		/* Looping till priority queue becomes empty (or all
-		distances are not finalized) */
-		while (true) {
-			
+			_visited[from] = true;
 
-			// 'i' is used to get all adjacent vertices of a
-			// vertex
-			for (link_t& i : adj[u]) {
-				// Get vertex label and weight of current
-				// adjacent of u.
-				int v = i.first;
+			for (const link_t i : get_adj(from)) {
+				int to = i.first;
 				int weight = i.second;
 
-				// If there is shorted path to v through u.
-				if (can_move(prev, u, v, col_size) && dist[v] > dist[u] + weight) {
-					// Updating distance of v
-					dist[v] = dist[u] + weight;	
-					prev[v] = u;
+				if (!_visited[to]
+					&& _dist[from] + weight < _dist[to]
+					/* && can_move(_prev, from, to, _col_size)*/) {
 
-					pq.push({ dist[v], v });
+					_dist[to] = _dist[from] + weight;
+					_prev[to] = from;
 
-					//print_path(map, prev, v);
-					//cout << endl << endl;
 				}
 			}
 		}
 
-		return dist;
+		return _dist;
 	}
 
-	vector<int> dist;
-	vector<int> prev;
-	vector<bool> visited;
+	void print() {
+		for (auto& nodes : adj) {
+			auto coord = get_coords(nodes.first, _col_size);
+			printf("Node %d (%d, %d): \n", nodes.first, coord.first, coord.second);
+			for (auto& node : nodes.second) {
+				coord = get_coords(node.first, _col_size);
+				printf("\t %d -> (%d, %d)\n", node.second, coord.first, coord.second);
+			}
+			printf("\n");
+		}
+	}
 
-	map_t map;
+	vector<int> _dist;
+	vector<int> _prev;
+	vector<bool> _visited;
+	int _col_size; //little hack
 };
 
 
@@ -202,8 +251,8 @@ void link(graph_t& graph, const map_t& map, int row, int col, int nextrow, int n
 	if (!is_outside(map, nextrow, nextcol)) {
 		int id = get_id(row, col, ncols);
 		int new_id = get_id(nextrow, nextcol, ncols);
+		
 		graph.add(id, new_id, map[nextrow][nextcol]);
-		//graph.add(new_id, id, map[row][col]);
 	}
 }
 
@@ -229,10 +278,10 @@ int main() {
 	input.close();
 
 	graph_t graph(map.size() * map[0].size());
-	
-	graph.map = map;
 
 	col_size = map[0].size();
+
+	graph._col_size = col_size;
 
 	const int nrows = map.size();
 	const int ncols = map[0].size();
@@ -255,12 +304,16 @@ int main() {
 	
 		}
 	}
+	
+	//cout << endl;
+	//graph.print();
+	//cout << endl;
 
 	vector<int> path = graph.path(0);
-	cout << "Min loss : " << path[map.size() * map[0].size() - 1] << endl;
-	//cout << "Min loss2: " << get_loss(map, graph.prev, map.size() * map[0].size() - 1) << endl;
+	cout << "Min loss (modified) : " << path[map.size() * map[0].size() - 1] << endl;
+	cout << "Min loss (actual): " << get_loss(map, graph._prev, map.size() * map[0].size() - 1) << endl;
 
-	print_path(map, graph.prev, map.size() * map[0].size() - 1);
+	print_path(map, graph._prev, map.size() * map[0].size() - 1);
 
 	return 0;
 }
